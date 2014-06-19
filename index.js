5235519fbfa64b22c6e559879867dc29cbc4e7cf
gaohuia <www@zeroplace.cn>
2014-06-19 19:04:18 +0800

    文件名宽度固定

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var Downloader = require("./Downloader");
 var alibaba = require("./alibaba");
 
+function getFormatNumber(i)
+{
+	var str = '' + i;
+	var arr = new Array(2-str.length);
+	arr.push(str);
+	return arr.join('0');
+}
+
 process.argv.slice(2).forEach(function(val, index, array) {
 	var conentPromise = alibaba.getImageArray(val);
 	conentPromise.then(function(imgArr){
@@ -8,7 +16,7 @@ process.argv.slice(2).forEach(function(val, index, array) {
 		for(i in imgArr) {
 			var src = imgArr[i];
 			
-			var tool = new Downloader(src, "result/" + i + ".jpg");
+			var tool = new Downloader(src, "result/" + getFormatNumber(i) + ".jpg");
 			tool.start();
 		}
 	});
@@ -18,7 +26,7 @@ process.argv.slice(2).forEach(function(val, index, array) {
 		for(i in imgArr) {
 			var src = imgArr[i];
 			
-			var tool = new Downloader(src, "big/" + i + ".jpg");
+			var tool = new Downloader(src, "big/" + getFormatNumber(i) + ".jpg");
 			tool.start();
 		}
 	});
